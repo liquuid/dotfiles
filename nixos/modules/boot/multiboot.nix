e9dacf00d26f03193a0a49f0603cdb430548686c
Fernando Henrique <liquuid@gmail.com>
2020-11-07 11:28:21 -0300

    remove nixos

--- nixos/modules/boot/multiboot.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  boot.loader = {
-    grub.useOSProber = true;
-  };
-  environment.systemPackages = with pkgs;
-    [ os-prober ];
-}
--- nixos/modules/hardware/sound.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }:
-{
-  hardware = {
-    pulseaudio.enable = true;
-  };
-  sound.enable = true;
-
-  # nixpkgs.config.pulseaudio = true;
-}
